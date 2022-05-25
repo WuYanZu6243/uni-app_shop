48ee893976ae80c4e9bc22c684f4ce9a8aca7a47
李文艺 <1369009517@qq.com>
2022-05-25 16:43:04 +0800

    完成了商品列表功能

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- 单个商品组件 -->
+  <view class="goods-item" @click="goodsHandler">
+    <!-- 商品图片 -->
+    <view class="goods-image">
+      <image :src="goods.goods_small_logo || defaultPic"></image>
+    </view>
+    <!-- 商品信息 -->
+    <view class="goods-info">
+      <text>{{goods.goods_name}}</text>
+      <text class="goods-price">￥{{goods.goods_price | tofixed}}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+	export default {
+    // 过滤器
+    filters: {
+      // 把数字处理为带两位小数点的数字
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    },
+    // 父组件的传参
+    props:{
+      // 商品对象信息
+      goods:{
+        type:Object,
+        defaul: {}
+      }
+    },
+    // 数据
+		data() {
+			return {
+				// 默认的空图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+    // 方法
+    methods:{
+      // 调用父组件绑定的点击事件
+      goodsHandler(){
+        this.$emit('click')
+      }
+    }
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+  width: 100%;
+  border: 1px solid #D3D3D3;
+  box-shadow: 1px 1px 2px #D3D3D3;
+  margin: 5px 0;
+  display: flex;
+  .goods-image{
+    margin-right: 5px;
+    image{
+      width: 180rpx;
+      height: 180rpx;
+    }
+  }
+  .goods-info{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 28rpx;
+    .goods-price{
+      font-size: 30rpx;
+      color: #c00000;
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,19 +1,95 @@
 <template>
-	<view>
-		goods_list
+	<view class="goods-list-content">
+    <!-- 商品列表 -->
+    <view class="goods-list">
+      <!-- 循环生成多个商品组件 -->
+      <block v-for="item in goodsList" :key="item.goods_id">
+        <my-goods :goods="item" @click='skipDetail(item)'></my-goods>
+      </block>
+    </view>
 	</view>
 </template>
 
 <script>
 	export default {
+    // 数据
 		data() {
 			return {
-				
+				// 数据请求参数
+        queryInfo:{
+          query:'',
+          cid:'',
+          pagenum:1,
+          pagesize:15
+        },
+        // 商品列表数据
+        goodsList:[],
+        // 商品列表商品总数
+        total:[],
+        // 节流阀,是否正在发送请求
+        isLoading : false
 			};
-		}
+		},
+    // 生命周期钩子
+    onLoad(options){
+      // 将页面参数转存到数据中
+      this.queryInfo.query = options.query || ''
+      this.queryInfo.cid = options.cid || ''
+      // 调用‘获取商品信息列表’方法
+      this.getGoodsList()
+    },
+    // 上拉触底事件
+    onReachBottom(){
+      // 加载下一页数据
+      // 判断是否还有下一页数据
+      if (this.queryInfo.pagenum * this.queryInfo.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      // 判断当前能不能发送请求
+      if(this.isLoading) return
+      // 页码+1
+      this.queryInfo.pagenum += 1 
+      // 重新发送请求获取商品列表数据
+      this.getGoodsList()
+    },
+    // 下拉刷新事件
+    onPullDownRefresh(){
+      // 重置参数
+      this.queryInfo.pagenum = 1
+      this.goodsList = []
+      this.total = 0
+      this.isLoading = false
+      // 重新发送数据请求
+      this.getGoodsList(()=>uni.stopPullDownRefresh())
+    },
+    // 方法
+     methods:{
+      // 获取商品信息列表
+      async getGoodsList(cb){
+        // 设置请求阀状态，表示当前正在发送请求
+        this.isLoading = true
+        // 发送请求
+        const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryInfo)
+        // 请求失败
+        if(res.meta.status !== 200) return uni.$showMsg()
+        // 请求成功
+        this.goodsList = [...this.goodsList,...res.message.goods]
+        this.total = res.message.total
+        // 设置请求阀状态，表示当前不在发送请求
+        this.isLoading = false
+        // cb是作用为让下拉框关闭的的函数
+        cb && cb()
+      },
+      // 跳转到详情页面
+      skipDetail(item){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+     }
 	}
 </script>
 
 <style lang="scss">
-
+  .goods-list{
+    padding: 5px;
+  }
 </style>
